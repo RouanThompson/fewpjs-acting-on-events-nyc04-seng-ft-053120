b07824054410b69847eef88e57179e7ffebebe15
Rouan Thompson <rouan.thompson@gmail.com>
2020-08-04 17:13:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        //have to convert it back to a string because the values are strings
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    } else if(e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
